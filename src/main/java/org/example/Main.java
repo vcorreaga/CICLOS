e6699bc414165a70aebdc9a2cc8e1242ae33f499
vcorrega <viadis0985@hotmail.com>
2025-05-21 20:40:14 -0500

    actualizacion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,18 +4,46 @@ public class Main {
     public static  void main(String [] args){
         Scanner sc = new Scanner(System.in);
         double nota = 0, acumulado  = 0, cantidadNotas = 0;
-        System.out.print("Ingrese la cantidad de notas a promediar: ");
-        cantidadNotas = sc.nextDouble();
+//        System.out.print("Ingrese la cantidad de notas a promediar: ");
+//        cantidadNotas = sc.nextDouble();
         System.out.println("Promedio de notas Logica programcion");
 
-        /*for(int i = 1; i <= cantidadNotas; i++){
+        /* Ciclo
+        for(int i = 1; i <= cantidadNotas; i++){
             System.out.print("Ingrese la nota" + i + ": ");
             nota = sc.nextDouble();
             acumulado+=nota;
 
         }*/
+        /*Ciclo while por defecto (cuando se sabe el numero de repeticiones)
 
-        System.out.print("El promedioo del estudiante es: " + (acumulado/cantidadNotas));
+        int i = 1;
+        while (i <= cantidadNotas){
+            System.out.print("Ingrese la nota " + i + ": ");
+            nota = sc.nextDouble();
+            acumulado += nota;
+            i++;
+        }
+
+        /*Ciclo controlado por un centinela, bandera o interruptor (cuando no se sabe la cantidad de veces a repetir)*/
+
+        int centinela = 1;
+        int opcion = 0;
+        int i = 1;
+        while (centinela == 1){
+            System.out.print("Ingrese la nota " + i + ": ");
+            nota = sc.nextDouble();
+            acumulado += nota;
+            i++;
+            System.out.println("Desea registrar otra nota? \n1 - Si\n0 - No ");
+            opcion = sc.nextInt();
+            if (opcion == 0){
+                centinela = 0;
+
+            }
+        }
+        System.out.print(i);
+        System.out.print("El promedioo del estudiante es: " + (acumulado/(i-1)));
     }
 }
 
@@ -30,4 +58,24 @@ for(int i = 1; i <= 5; i++){
             acumulado+=nota;
 
         }
+        System.out.print("El promedioo del estudiante es: " + (acumulado/cantidadNotas));
+
+
+        int centinela = 1;
+        int opcion = 0;
+        int i = 1;
+        while (centinela == 1){
+            System.out.print("Ingrese la nota " + i + ": ");
+            nota = sc.nextDouble();
+            acumulado += nota;
+            i++;
+            System.out.println("Desea registrar otra nota? \n1 - Si\n0 - No ");
+            opcion = sc.nextInt();
+            if (opcion == 0){
+                centinela = 0;
+
+            }
+        }
+        System.out.print(i);
+        System.out.print("El promedioo del estudiante es: " + (acumulado/i));
 */
